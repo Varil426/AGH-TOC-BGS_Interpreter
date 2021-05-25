25b34f75bb022b017c67f483da49e14a0618e60c
Bartlomiej Kregielewski <bartlomiejkregielewski@gmail.com>
2021-05-25 12:39:04 +0200

    Add project files.

---
+++ BGS_Interpreter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BGS_Interpreter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
